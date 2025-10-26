30784c09bcfec5a5289ccf48342561d71a468655
윤민수 <koelysa5696@naver.com>
2025-10-26 18:32:34 +0900

    feat: 경주 시작 기능 구현
    - 라운드를 인자로 받고 경주 시작
    - 경주의 결과를 받아와 출력
    - 우승자 목록을 받아와 출력

---
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -0,0 +1,39 @@
+package racingcar.domain;
+
+import static racingcar.constants.GameGuideMessage.PRINT_GAME_WINNERS_MESSAGE;
+import static racingcar.constants.Regex.NAME_SPLIT;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingGame {
+    private final Cars cars;
+    private final List<List<String>> raceHistory = new ArrayList<>();
+
+    public RacingGame(Cars cars) {
+        this.cars = cars;
+    }
+
+    public void startRace(int round) {
+        for (int i = 0; i < round; i++) {
+            cars.moveCars();
+            raceHistory.add(cars.toStringAll());
+        }
+    }
+
+    public void raceResultPrint() {
+        for (List<String> round : raceHistory) {
+            round.forEach(System.out::println);
+            System.out.println();
+        }
+    }
+
+    public void printWinners(Cars cars) {
+        List<String> winners = cars.findWinners();
+        String result = String.join(NAME_SPLIT.getRegex(), winners);
+        System.out.println(PRINT_GAME_WINNERS_MESSAGE.getMessage() + result);
+    }
+
+
+}
+
