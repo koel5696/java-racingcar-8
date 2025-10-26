d2342ee52dfecc31cd8f2560f105f918728ebf18
윤민수 <koelysa5696@naver.com>
2025-10-26 18:35:38 +0900

    feat: 정규식 관리 상수 클래스
    - 이름 입력 상수
    - 진행 횟수 입력 상수
    - 이름 구분 상수
    - 개행 문자 상수
    - 전진 구분자 상수

---
+++ src/main/java/racingcar/constants/Regex.java
@@ -0,0 +1,19 @@
+package racingcar.constants;
+
+public enum Regex {
+    NAME_INPUT("^[a-zA-Z가-힣]+$"),
+    ROUND_INPUT("^[1-9]\\d*$"),
+    NEWLINE("\n"),
+    NAME_SPLIT(","),
+    FORWARD_EXPRESS("-");
+
+    private final String regex;
+
+    Regex(String regex) {
+        this.regex = regex;
+    }
+
+    public String getRegex() {
+        return regex;
+    }
+}
