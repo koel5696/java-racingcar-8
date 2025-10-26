375022e36f5b0fdb45f99dca1c23388f3347360f
윤민수 <koelysa5696@naver.com>
2025-10-26 18:24:50 +0900

    test: 자동차 관리 테스트 작성
    - 자동차 전진 확인 테스트
    - 우승자 확인 테스트

---
+++ src/test/java/racingcar/domain/CarTest.java
@@ -0,0 +1,37 @@
+package racingcar.domain;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+    private static final int MAX_DISTANCE = 5;
+    private static final int MOVING_FORWARD = 4;
+
+    @Test
+    void 전진_횟수_증가_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            Car car = new Car("benz");
+            car.goOrStop();
+            assertThat(car.toString()).isEqualTo("benz : -");
+        }, MOVING_FORWARD);
+    }
+
+    @Test
+    void 우승자_확인_테스트() {
+        assertRandomNumberInRangeTest(() -> {
+            Car car = new Car("benz");
+
+            for (int i = 0; i < MAX_DISTANCE; i++) {
+                car.goOrStop();
+            }
+
+            Optional<String> winner = car.announceIfWinner(MAX_DISTANCE);
+
+            assertThat(winner).hasValue("benz");
+        }, MOVING_FORWARD);
+    }
+
+}
