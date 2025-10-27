b0c60ae305906834a417fb5332802b70f655f7c6
윤민수 <koelysa5696@naver.com>
2025-10-27 20:33:24 +0900

    test: 자동차 멈춤 테스트 추가 작성

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,6 +1,5 @@
 package racingcar.domain;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Optional;
@@ -9,28 +8,42 @@ import org.junit.jupiter.api.Test;
 public class CarTest {
     private static final int MAX_DISTANCE = 5;
     private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
 
     @Test
     void 전진_횟수_증가_테스트() {
-        assertRandomNumberInRangeTest(() -> {
-            Car car = new Car("benz");
-            car.goOrStop();
-            assertThat(car.toString()).isEqualTo("benz : -");
-        }, MOVING_FORWARD);
+        Car car = new Car("benz");
+        for (int i = 0; i < 5; i++) {
+            car.goOrStop(MOVING_FORWARD);
+        }
+        assertThat(car.toString()).isEqualTo("benz : -----");
+
     }
 
     @Test
-    void 우승자_확인_테스트() {
-        assertRandomNumberInRangeTest(() -> {
-            Car car = new Car("benz");
-
-            for (int i = 0; i < MAX_DISTANCE; i++) {
-                car.goOrStop();
+    void 멈춤_테스트() {
+        Car car = new Car("benz");
+        for (int i = 0; i < 5; i++) {
+            if (i == 3) {
+                car.goOrStop(STOP);
+                continue;
             }
+            car.goOrStop(MOVING_FORWARD);
+        }
+        assertThat(car.toString()).isEqualTo("benz : ----");
 
-            Optional<String> winner = car.saveWinners(MAX_DISTANCE);
+    }
 
-            assertThat(winner).hasValue("benz");
-        }, MOVING_FORWARD);
+    @Test
+    void 우승자_목록_최종_저장_테스트() {
+        Car car = new Car("benz");
+
+        for (int i = 0; i < MAX_DISTANCE; i++) {
+            car.goOrStop(MOVING_FORWARD);
+        }
+
+        Optional<String> winner = car.saveWinners(MAX_DISTANCE);
+        assertThat(winner).hasValue("benz");
+
     }
 }
